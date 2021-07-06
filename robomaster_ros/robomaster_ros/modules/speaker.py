c4a5375d3ed6c7815bb45a0fcf7ab72b5fb4dfa0
Jerome Guzzi <jerome@idsia.ch>
2021-07-06 09:19:18 +0200

    Added submask. Added playing audio file

--- robomaster_ros/robomaster_ros/modules/speaker.py
+++ robomaster_ros/robomaster_ros/modules/speaker.py
@@ -1,10 +1,12 @@
+import os
+
 import rclpy.action
 import robomaster.robot
 import robomaster_msgs.msg
 
 from ..action import add_cb
 
-from typing import Any
+from typing import Any, Dict
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from ..client import RoboMasterROS
@@ -19,14 +21,48 @@ class Speaker(Module):
         self._sound_action_server = rclpy.action.ActionServer(
             node, robomaster_msgs.action.PlaySound, "play", self.execute_play_sound
         )
+        self._audio_files: Dict[int, str] = {}
+        self._audio_id = 0
+
+    # Using this version as it caches the files/sound_id
+    def load_audio(self, filename: str) -> int:
+        filename = os.path.abspath(filename)
+        for audio_id, name in self._audio_files.items():
+            if filename == name:
+                return 0xE0 + audio_id
+        if not os.path.exists(filename):
+            self.logger.warning(f"Loading audio: file {filename} does not exists")
+            return -1
+        if self.api._ftp.connected:
+            self._audio_id = (self._audio_id + 1) % 10
+            self._audio_files[self._audio_id] = filename
+            upload_file = f"/python/sdk_audio_{self._audio_id}.wav"
+            self.logger.info(f"Loading audio: file {filename} as sound {0xE0 + self._audio_id}")
+            self.api._ftp.upload(filename, upload_file)
+            return 0xE0 + self._audio_id
+        else:
+            return -1
 
     def stop(self) -> None:
         self._sound_action_server.destroy()
 
     def execute_play_sound(self, goal_handle: Any) -> robomaster_msgs.action.PlaySound.Result:
-        self.logger.info("[Action begin] play sound")
         request = goal_handle.request
-        action = self.api.play_sound(sound_id=request.sound_id, times=request.times)
+        if request.file:
+            sound_id = self.load_audio(request.file)
+        else:
+            sound_id = request.sound_id
+        if sound_id < 0:
+            goal_handle.abort()
+            self.logger.warning('Cannot play sound')
+            return robomaster_msgs.action.PlaySound.Result()
+        try:
+            action = self.api.play_sound(sound_id=sound_id, times=request.times)
+        except RuntimeError as e:
+            self.logger.warning(f'Cannot play sound: {e}')
+            goal_handle.abort()
+            return robomaster_msgs.action.PlaySound.Result()
+        self.logger.info(f"Start playing sound with request {request}")
         feedback_msg = robomaster_msgs.action.PlaySound.Feedback()
         feedback_msg.time = 0
         progress = [2.0]
@@ -41,4 +77,5 @@ class Speaker(Module):
         add_cb(action, cb)
         action.wait_for_completed()
         goal_handle.succeed()
+        self.logger.info("Done playing sound")
         return robomaster_msgs.action.PlaySound.Result()
