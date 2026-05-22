8e097eb9f2ae3667c079e1d5a1f641407b5d1c59
Jérôme Guzzi <jerome@idsia.ch>
2026-05-22 13:49:51 +0200

    Cleaned up shutdown
    Changed to avoid running `rclpy.spin_once` in a background thread

--- robomaster_ros/robomaster_ros/robomaster_driver.py
+++ robomaster_ros/robomaster_ros/robomaster_driver.py
@@ -16,27 +16,20 @@ def main(args: Any = None) -> None:
     # TODO(Jerome): currently not triggered by ctrl+C
     # rclpy.get_default_context().on_shutdown(...)
     should_reconnect = True
+    running = True
 
     node: Optional[RoboMasterROS] = None
 
     def shutdown(sig, _):
         nonlocal should_reconnect
-        should_reconnect = False
-        if node:
-            node.stop_heartbeat_check()
-            if rclpy.ok():
-                node.abort()
-                rclpy.spin_once(node, executor=executor, timeout_sec=0.1)
-            if rclpy.ok():
-                node.stop()
-                rclpy.spin_once(node, executor=executor, timeout_sec=0.1)
-        else:
+        nonlocal running
+        running = False
+        if not node:
             raise KeyboardInterrupt
-        rclpy.try_shutdown()
 
     signal.signal(signal.SIGINT, shutdown)
 
-    while should_reconnect and rclpy.ok():
+    while should_reconnect and rclpy.ok() and running:
         try:
             node = RoboMasterROS(executor=executor)
         except KeyboardInterrupt:
@@ -44,14 +37,14 @@ def main(args: Any = None) -> None:
         should_reconnect = node.reconnect
         if not node.disconnection.done():
             try:
-                while rclpy.ok() and not node.disconnection.done():
+                while rclpy.ok() and not node.disconnection.done() and running:
                     rclpy.spin_until_future_complete(
                         node, node.disconnection, executor=executor, timeout_sec=1.0)
             except KeyboardInterrupt:
-                should_reconnect = False
+                running = False
                 break
             except rclpy._rclpy_pybind11.RCLError:
-                should_reconnect = False
+                running = False
                 break
         node.stop_heartbeat_check()
         if rclpy.ok():
